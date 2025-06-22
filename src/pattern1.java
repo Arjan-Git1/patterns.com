d55d59b5e3bda88b5f4ff91d8177e94868d6c6af
Arjan Chakrabory <aryaarjan1@gmail.com>
2025-06-22 18:33:44 +0530

    add my files~

---
+++ src/pattern1.java
@@ -0,0 +1,9 @@
+public class pattern1 {
+    public static void main(String[] args){
+        int d=1;
+        for(int i=1;i<=11111;){
+            i=i*10+d;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/pattern2.java
@@ -0,0 +1,10 @@
+public class pattern2 {
+    public static void main(String[] args){
+        int d=2;
+        int i=1;
+        for(;d<=5;d++){
+            i=i*10+d;
+            System.out.println(i);
+        }
+    }
+}
